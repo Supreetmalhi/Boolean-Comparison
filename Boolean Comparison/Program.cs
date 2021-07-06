2667fdbc5dd90e0d59757984dfb009392ada83d0
Supreetmalhi <malhisupreet40@gmail.com>
2021-07-05 22:50:36 -0400

    Initial commit

---
+++ Boolean Comparison/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Boolean_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While Loop Example");
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("Do While Loop Example");
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j < 5);
+        }
+    }
+}
